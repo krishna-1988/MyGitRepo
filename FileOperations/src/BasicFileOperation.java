4d778aeba84c3bdbb49d5e471b6212391792fac1
Krishna <alasai@outlook.com>
2015-12-31 19:29:33 +0530

    Added fileOperation

---
+++ FileOperations/src/BasicFileOperation.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Sai Krishna on 31-12-2015.
+ */
+public class BasicFileOperation {
+    int a;
+    int b;
+    int sum(int a,int b) {
+        this.a = a;
+        this.b = b;
+        int sumValue = this.a+this.b;
+        return sumValue;
+    }
+}
