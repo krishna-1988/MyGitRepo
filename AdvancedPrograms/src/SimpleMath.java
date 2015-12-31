563f43d3103a0abb3b5a1c96cd7a6acb0d8fd145
Krishna <alasai@outlook.com>
2015-12-31 19:19:22 +0530

    Advanced Module Commit

---
+++ AdvancedPrograms/src/SimpleMath.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Sai Krishna on 31-12-2015.
+ */
+public class SimpleMath {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+        System.out.println("The sum is:"+(a+b));
+    }
+}
---
+++ AdvancedPrograms/src/sum/MathOperations.java
@@ -0,0 +1,13 @@
+package sum;
+
+/**
+ * Created by Sai Krishna on 31-12-2015.
+ */
+public class MathOperations {
+    public static void main(String[] args){
+        int a = 10;
+        int b = 5;
+        System.out.println("The sum is: "+(a+b));
+        System.out.println("The difference is: "+(a-b));
+    }
+}
