064c567e90741ff0e8e40a796874fe48d4bf85aa
Krishna <urssaikrishna@yahoo.com>
2016-01-01 19:56:58 +0530

    Changed Name of class file

---
+++ AdvancedPrograms/src/sum/GetValues.java
@@ -0,0 +1,11 @@
+package sum;
+//import BasicPrograms.*;
+
+/**
+ * Created by Sai Krishna on 31-12-2015.
+ */
+public class GetValues {
+    public static void main(String[] args){
+      // MathOperations mathOperations = new MathOperationsImpl();
+    }
+}
