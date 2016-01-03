c4a7c45ec1bce8adf40dbe9a2cd59c748fd1bbca
Krishna <urssaikrishna@yahoo.com>
2016-01-03 18:54:32 +0530

    Added product statement

--- AdvancedPrograms/src/SimpleMath.java
+++ AdvancedPrograms/src/SimpleMath.java
@@ -5,6 +5,7 @@ public class SimpleMath {
     public static void main(String[] args) {
         int a = 10;
         int b = 5;
-        System.out.println("The sum is:"+(a+b));
+        System.out.println("The sum is: "+(a+b));
+        System.out.println("The product is: "+(a*b));
     }
 }
